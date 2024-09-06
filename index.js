8bfcc27ab06b439c75a90af54e8dde093ec7f68a
akashSingh <singhsahab2k3@gmail.com>
2024-09-06 13:27:49 +0530

    changes for component

--- index.js
+++ index.js
@@ -98,31 +98,31 @@ app.post("/webhook", (req, res) => {
                         name: "welcome_msg",
                         language: {
                             code: "en"
-                        },
-                        components: [
-                            {
-                                type: "button",
-                                sub_type: "quick_reply",
-                                index: 0,
-                                parameters: [
-                                    {
-                                        type: "text",
-                                        text: "Yes"
-                                    }
-                                ]
-                            },
-                            {
-                                type: "button",
-                                sub_type: "quick_reply",
-                                index: 1,
-                                parameters: [
-                                    {
-                                        type: "text",
-                                        text: "No"
-                                    }
-                                ]
-                            }
-                        ]
+                        }
+                        // components: [
+                        //     {
+                        //         type: "button",
+                        //         sub_type: "quick_reply",
+                        //         index: 0,
+                        //         parameters: [
+                        //             {
+                        //                 type: "text",
+                        //                 text: "Yes"
+                        //             }
+                        //         ]
+                        //     },
+                        //     {
+                        //         type: "button",
+                        //         sub_type: "quick_reply",
+                        //         index: 1,
+                        //         parameters: [
+                        //             {
+                        //                 type: "text",
+                        //                 text: "No"
+                        //             }
+                        //         ]
+                        //     }
+                        // ]
                     }
 
 
